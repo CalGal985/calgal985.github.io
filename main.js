d015ff603a4855746f96ace2331b177f2cf1735f
Callie Frogburg <calgal985@gmail.com>
2024-05-16 13:56:14 +0000

    added so much

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
+
+let webBody = document.querySelector("body");
+
+webBody.src = '/assets/salvage/LightBeam.png';
